6390cdb5cc975276f69003bac097ca68240b3a4f
Alfredoeb9 <alfredoeb96@gmail.com>
2024-08-14 15:33:55 -0700

    [prod] removed model import and changed load_env to pyton-dotenv

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from sqlalchemy import select
-from models.models import Item
+# from models.models import Item
 
 # load_dotenv()
 
