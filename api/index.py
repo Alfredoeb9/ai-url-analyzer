aef9903704646b3b5cc2c5bfcfe4a1912a5f9af2
Alfredoeb9 <alfredoeb96@gmail.com>
2024-08-22 21:36:46 -0700

    [cors] implemented api

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, jsonify
-# from flask_cors import CORS
+from flask_cors import CORS
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
